74d1a3495422c93b45d2237da5926920fc0a62b2
Ma Tingchen <defmys@gmail.com>
2017-01-30 15:53:21 +0800

    Add exec3_3

---
+++ chapter3/exec3_3/exec3_3.go
@@ -0,0 +1,89 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 58.
+//!+
+
+// Surface computes an SVG rendering of a 3-D surface function.
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	peek, valley := findPeekValley()
+
+	colorScale := float64(0xFF) / (peek - valley)
+
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, _ := corner(i+1, j)
+			bx, by, z := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, _ := corner(i+1, j+1)
+			colorOffset := int((z - valley) * colorScale)
+			fmt.Printf("<polygon fill='#%02X00%02X' points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+				colorOffset, 0xFF-colorOffset, ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Println("</svg>")
+}
+
+func findPeekValley() (float64, float64) {
+	peek := math.Inf(-1)
+	valley := math.Inf(1)
+	for i := 0; i <= cells; i++ {
+		for j := 0; j <= cells; j++ {
+			_, _, z := corner(i, j)
+			if z > peek {
+				peek = z
+			}
+			if z < valley {
+				valley = z
+			}
+		}
+	}
+	return peek, valley
+}
+
+func calcxyz(i, j int) (float64, float64, float64) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+
+	return x, y, z
+}
+
+func corner(i, j int) (float64, float64, float64) {
+	x, y, z := calcxyz(i, j)
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, z
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	return math.Sin(r) / r
+}
+
+//!-
